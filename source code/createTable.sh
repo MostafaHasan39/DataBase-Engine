b9073c4b6dc1bc46360a9c4198ff758e70c6701d
MostafaHasan39 <mostafahasan.eng@gmail.com>
2019-03-08 18:43:15 -0500

    modification to createTable.sh

--- source code/createTable.sh
+++ source code/createTable.sh
@@ -1,73 +1,31 @@
 #!/bin/bash
 
 dbName=$1;
-validator = ^[a-zA-Z][0-9|a-z|A-Z|_|\d]*$
 
 while true
+  do
+  clear
+  echo -e "--------You are now using $dbName DataBae --------\n\n\n"
+  printf "Please enter the new table name : \n"
+  read tableName
 
-do
-clear
-echo -e "--------You are now using $dbName DataBae --------\n\n\n"
-printf "Please enter the new table name : \n"
-read tableName
 
-
-if [[ "$tableName" =~ ^[a-zA-Z][0-9|a-z|A-Z|_|\d]*$ ]]
-then 
-  if [ -e ./DBs/$dbName/$tableName ]
+  if [[ "$tableName" =~ ^[a-zA-Z][0-9|a-z|A-Z|_|\d]*$ ]]
   then 
-  echo "This table is already existed"
-  sleep 5
+      if [ -e ../DBs/$dbName/$tableName ]
+      then 
+        echo "This table is already existed"
+        sleep 2
+      else 
+        touch ../DBs/$dbName/$tableName
+        echo "Table Created Successfully"
+         sleep 2
+         break
+      fi 
   else 
-  touch ./DBs/$dbName/$tableName
-  echo "This is New Table"
-  sleep 5
-  break
-  fi 
-else 
-  echo "The table name must start with alphapet"
-  sleep 5
-fi
+    echo "The table name must start with alphapet"
+    sleep 2
+  fi
 done
 
 . tableMenu.sh
-
-
-
-
-
-
-
-#Table Name Validation 
-#if [ -e ./DBs/$dbName/$tableName ]
-#then 
-#echo "This table is already existed"
-#sleep 2
-#else 
-#touch ./DBs/$dbName/$tableName
-#echo "This is New Table"
-#break
-#fi
-#done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
